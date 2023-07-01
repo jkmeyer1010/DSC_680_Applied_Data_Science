d80eb214e60b3f0a9f8aeebaea8b75351604b202
Jake Meyer <jakmeyer@my365.bellevue.edu>
2023-06-30 23:50:30 -0600

    Portfolio Milestone 1
    Portfolio organization and review.

---
+++ Markdown_to_pdf.py
@@ -0,0 +1,3 @@
+import aspose.words as aw
+doc = aw.Document("README_Sales_Forecasting_and_Late_Delivery_Prediction_Example.md")
+doc.save(f"README_Sales_Forecasting_and_Late_Delivery_Prediction_Example.pdf")
